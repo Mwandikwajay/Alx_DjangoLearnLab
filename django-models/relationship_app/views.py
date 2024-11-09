f9243a88ec146fc1ba8cd2886cb09f2757a69057
Mwandikwajay <musangij@gmail.com>
2024-11-09 10:52:08 +0300

    Resolved conflict

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,4 +4,14 @@ from relationship_app.models import Book
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
-    return render(request, 'list_books.html', {'books': books})
+
+    
+from django.shortcuts import render
+from django.views.generic import DetailView
+from relationship_app.models import Library
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Make sure the template is correctly referenced
+    context_object_name = 'library'
