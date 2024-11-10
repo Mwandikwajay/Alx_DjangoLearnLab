ed0916de1e0150790dbed10fcc28c568f5cd9346
Mwandikwajay <musangij@gmail.com>
2024-11-10 15:33:50 +0300

    Updated url py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    # Secured views for adding, editing, and deleting books
-    path('add/', views.add_book, name='add_book'),  # Secured add book view
-    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),  # Secured edit book view
-    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),  # Secured delete book view
+    # Custom Permissions urls
+    path("books/add_book/", views.add_book, name="add_book"),
+    path("books/edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("books/delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
