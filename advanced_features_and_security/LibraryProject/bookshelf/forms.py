239e38480948a4dab012e6bd75d5d576c49156a8
Mwandikwajay <musangij@gmail.com>
2024-11-16 16:09:16 +0300

    Updated forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+        
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    author = forms.CharField(max_length=100)
+    publication_year = forms.IntegerField()
