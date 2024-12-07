692fff86a3b483b378d8af8474060578c51e7f8b
Mwandikwajay <musangij@gmail.com>
2024-12-07 16:12:16 +0300

    Updated formspy

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Comment
-from .models import Post
+from .models import Comment, Post
+from taggit.forms import TagWidget  # Import TagWidget for tags
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -18,7 +18,11 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment here...'}),
         }
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Include 'tags' field        
+        fields = ['title', 'content', 'tags']  # Include 'tags' field
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for tags
+        }
